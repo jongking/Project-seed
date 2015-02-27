27ac2768eee84094870aa4df82140986b1e18d25
jongking <498160093@qq.com>
2015-02-27 22:44:00 +0800

    init NHibernate project-seed
    提交NHibernate的种子项目

---
+++ nhibernate/Domain/Model/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Model
+{
+    [Serializable]
+    public class Product
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Category { get; set; }
+        public virtual bool Discontinued { get; set; }
+    }
+}
---
+++ nhibernate/JHelper.Tests/GenerateSchema_Fixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain.Model;
+using JHelper;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace JHelper.Tests
+{
+    [TestFixture]
+    public class GenerateSchema_Fixture
+    {
+        [Test]
+        public void Can_generate_schema()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Product).Assembly);
+
+            new SchemaExport(cfg).Execute(true, true, true);
+        }
+    }
+}
